0a36b34725f63b6e85587f2220a73bce76ac2fbb
doug <doug@unlikelysource.com>
2016-09-19 15:15:30 +0700

    2016-09-19 DB: updated ecb XML example

--- web/xml_ecb_currency_conversion.php
+++ web/xml_ecb_currency_conversion.php
@@ -1,19 +1,41 @@
 <?php
-//This is a PHP (4/5) script example on how eurofxref-daily.xml can be parsed 
+//This is a PHP (4/5) script example on how eurofxref-daily.xml can be parsed
 
-//Read eurofxref-daily.xml file in memory 
-$XMLContent= file("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
-//the file is updated daily between 2.15 p.m. and 3.00 p.m. CET
+// the file is updated daily between 2.15 p.m. and 3.00 p.m. CET
+define('ECB_URL', 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml');
 
-// Parse output
-$rates = array();
-foreach ($XMLContent as $line) {
-   if (preg_match("/currency='([[:alpha:]]+)'/",$line,$currencyCode)) {
-       if (preg_match("/rate='([[:graph:]]+)'/",$line,$rate)) {
-          //Output the value of 1 EUR for a currency code 
-          $rates[$currencyCode[1]] = $rate[1];
-       }
-   }
+class Rate
+{
+    public $code;
+    public $rate;
+    public function __construct($code, $rate)
+    {
+        $this->code = $code;
+        $this->rate = $rate;
+    }
 }
-var_dump($rates);
-?> 
+
+function getEcbRates()
+{
+    //Read eurofxref-daily.xml file in memory
+    $XMLContent= file(ECB_URL);
+
+    // Parse output
+    $rates = array();
+    foreach ($XMLContent as $line) {
+       if (preg_match("/currency='([[:alpha:]]+)'/", $line, $currencyCode)) {
+           if (preg_match("/rate='([[:graph:]]+)'/", $line, $rate)) {
+              //Output the value of 1 EUR for a currency code
+              yield new Rate($currencyCode[1], $rate[1]);
+           }
+       }
+    }
+}
+
+$output = '<table>' . PHP_EOL;
+foreach (getEcbRates() as $rate) {
+    $output .= "<tr><td>{$rate->code}</td><td>{$rate->rate}</td></tr>\n";
+}
+$output .= '</table>' . PHP_EOL;
+echo $output;
+
