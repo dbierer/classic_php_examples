e13bfbad3c8ae31d72d12ee8658dfa2aed3d5ff7
dbierer <doug@unlikelysource.com>
2021-05-04 10:43:31 +0700

    2021-05-04 DB: Added bulk rename to

--- file/test/xyz_file_get_contents.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$contents = file_get_contents("http://www.google.com/");
-//echo htmlentities($contents);
-$contents = str_replace(
-	array("Google","Lucky"),
-	array("Boogle","Dizzy"),
-	$contents);
-echo $contents;
--- file/test/xyz_test.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Content-Type: application/mp4');
-readfile('name-of-the-file.mp4);
