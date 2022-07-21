68a463a8e604b8fc3565f4982ddb744fc1d8afc8
dbierer <doug@unlikelysource.com>
2022-07-21 21:20:35 +0700

    2022-07-21 DB: added examples for SPL and union types

--- oop/callable_examples.php
+++ oop/callable_examples.php
@@ -14,22 +14,42 @@ class Test
     {
         return array_sum($arr);
     }
+    public function __invoke($arr)
+    {
+        return array_sum($arr);
+	}
 }
 
+$anon = new class () {
+	public function __invoke($arr) {
+		return array_sum($arr);
+	}
+};
+
 $callback_tree = [
-    'anon_func' => function ($arr) { return array_sum($arr); },
-    'arrow_func' => fn ($arr) => array_sum($arr),
-    'anon_class' => new class () {
-        public function __invoke($arr) {
-            return array_sum($arr);
-        }
-    },
-    'php_func' => 'array_sum',
-    'obj_method' => [new Test(), 'sum'],
-    'static_method' => 'Test::static_sum',
+    'anon_func'      => function ($arr) { return array_sum($arr); },
+    'arrow_func'     => fn ($arr) => array_sum($arr),
+    'anon_class'     => $anon,
+    '__invoke'       => new Test(),
+    'php_func'       => 'array_sum',
+    'obj_method'     => [new Test(), 'sum'],
+    'static_method1' => 'Test::static_sum',
+    'static_method2' => [Test::class, 'static_sum'],
 ];
 
 $arr = [1,2,3,4,5,6,7,8,9];
 foreach ($callback_tree as $key => $item) {
-    printf("\n%14s: %3d", $key, $item($arr));
+    printf("\n%20s: %3d", $key, $item($arr));
 }
+
+/*
+ * Output:
+           anon_func:  45
+          arrow_func:  45
+          anon_class:  45
+            __invoke:  45
+            php_func:  45
+          obj_method:  45
+      static_method1:  45
+      static_method2:  45
+*/
--- test.php
+++ test.php
@@ -1,39 +1,22 @@
 <?php
-// Create database connection
-$pdo = include './db/get_pdo.php';
-
-// Open connection
-try {
-    // get list of product SKUs
-    $stmt = $pdo->query('SELECT sku FROM products');
-    $skus = array_column($stmt->fetchAll(PDO::FETCH_ASSOC), 'sku');
-    $max  = count($skus) - 1;
-    
-    // get list of customer IDs
-    $stmt = $pdo->query('SELECT id FROM customers');
-    $ids  = array_column($stmt->fetchAll(PDO::FETCH_ASSOC), 'id');
-
-    // SQL prepare
-    $sql = "UPDATE customers SET products_purchased = :d WHERE id = :i";
-    $sth = $pdo->prepare($sql);
-
-    foreach ($ids as $id) {
-        // get how many products
-        $num = random_int(1, 12);
-        $prods = [];
-        // build random array of products "purchased"
-        for ($x = 0; $x < $num; $x++) {
-            $prods[] = $skus[random_int(0, $max)];
-        }
-        $json = json_encode($prods);
-        echo $json . PHP_EOL;
-        $sth->execute(['d' => $json, 'i' => $id]);
-    }
-
-    $stmt = $pdo->query('SELECT * FROM customers');
-    var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
-    
-} catch (PDOException $e) {
-    echo $e->getMessage();
+class Test
+{
+	public int|float $status = 0;
 }
+$test = new Test();
+
+$test->status = 22;
+var_dump($test);
+
+$test->status = 22/7;
+var_dump($test);
+
+$test->status = FALSE;
+var_dump($test);
+
+$test->status = '22';
+var_dump($test);
+
+$test->status = 'TEST';
+var_dump($test);
 
